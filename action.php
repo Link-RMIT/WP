91ab988c0a95b2e9be753fcc65596c07604db636
Lucas O'hare <s3545867@csitprdap01.int.its.rmit.edu.au>
2016-05-11 11:56:22 +1000

    init

---
+++ action.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Silverado Cinema</title>
+
+    <style>
+     
+    </style>
+  <link rel="stylesheet" href="style.css" />
+ <link href='https://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+  </head> 
+
+
+  <body>
+	
+    <header class= "header">
+      <a href="index.php">
+      <div><img border="0" alt="home" src="logo.PNG" width=500 height=150/></div> 
+      </a>
+    </header>
+
+    <nav class="nav">
+      <a>Home</a> | <a href="sessions.php">Out Now</a> | <a href="contact.php">Contact Us</a>
+    </nav>
+
+    <main class="main">
+	<div id="box">
+	         <div class="box2"><a href="sessions.php">OUT NOW</a> | ACTION | <a href="art.php">ART/FOREIGN</a> | <a href="kids.php">KIDS</a> | <a href="romcom.php">ROMANTIC COMEDY</a></div>
+
+       		 <a href="batvsup.php"><div class="floating-box"><img src="batvsup.jpg"></div></a>
+	</div>
+	</br>
+	<div id="box"
+	<div class="discount"> ON MONDAYS AND TUESDAYS (ALL DAY) </br> AND ON MONDAYS TO FRIDAYS (1PM SESSION ONLY) </br>GET DISCOUNTED TICKETS!</br>
+	</div>
+
+    </main>
+
+    <footer class="footer">
+      &copy; Lucas O'Hare s3545867
+      <script>
+        document.write(new Date().getFullYear());
+      </script>.
+    </footer>
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Silverado Cinema</title>
+
+    <style>
+	details{ font-family: 'Oswald'}
+
+
+    </style>
+  <link rel="stylesheet" href="style.css" />
+  <link href='https://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+  </head> 
+
+
+
+  <body>
+	
+    <header class="header">
+      <a href="index.php">
+      <div><img border="0" alt="home" src="logo.PNG" width=500 height=150/></div> 
+      </a>
+      
+    </header>
+
+    <nav class="nav">
+      <a>Home</a> | <a href="sessions.php">Out Now</a> | <a href="contact.php">Contact Us</a>
+    </nav>
+
+    <main class="main">
+	<div id="box">
+
+		<table class="styledTable2">		
+			<tr>
+				<th valign="top">
+				<details>
+					<summary>Ticket Prices</summary>
+						*Beanbag price allows up to 2 adults OR 1 adult and 1 child OR up to 3 children. </br>One price fits all!
+						<details>
+							<summary>Mon-Tue(ALL DAY)/Mon-Fri(1PM)</summary>
+							Standard-Full: $12</br>
+							Standard-Child: $10</br>
+							Standard-Concession: $8</br>
+							First Class-Full: $25</br>
+							First Class-Child: $20</br>
+							*Beanbag: $20</br>
+						</details>
+						<details>
+							<summary>Wed-Fri(Not 1PM)/Sat-Sun(ALL DAY)</summary>
+							Standard-Full: $18</br>
+							Standard-Child: $15</br>
+							Standard-Concession: $12</br>
+							First Class-Full: $30</br>
+							First Class-Child: $25</br>
+							*Beanbag: $30</br>
+						</details>
+					
+				</details>
+
+				</br>
+				<details>
+					<summary>Weekly Schedule</summary>
+						<details>
+							<summary>Mon-Tue</summary>
+							1pm - The Jungle Book</br>
+							6pm - Ambarsariya</br>
+							9pm - How to be Single</br>
+						</details>
+						<details>
+							<summary>Wed-Fri</summary>
+							1pm - How to be Single</br>
+							6pm - The Jungle Book</br>
+							9pm - Batman v Superman: Dawn of Justice</br>
+						</details>
+						<details>
+							<summary>Sat-Sun</summary>
+							12pm - The Jungle Book</br>
+							3pm - Ambarsariya</br>
+							6pm - How to be Single</br>
+							9pm - Batman v Superman: Dawn of Justice</br>
+						</details>
+				</details>
+
+
+				</br></th>
+				<td>
+				Welcome to the Silverado Cinema! Here at our organisation we strive to 
+				give our customers the best viewing experience possible for the lowest
+				prices and the greatest value. </br>
+				<img src="cinema.jpg" style= width=480; height=350; float: right;/></br>
+				We have recently renovated our cinemas,upgrading our seating, giving our 
+				customers loads of leg room and the ultimate comfortable moovie experience.
+				We have installed state of the arc 3D projection facilities putting you inside
+				the movie to live it yourself!</br> <img src="3d.jpg" style=width=400; height=400; float:right;/></br>
+                                Finally after installing beautiful new Dolby lighting and sound,
+				we have combined the "most powerful image and sound technologies, to transform
+				your visit into a completely captivating cinematic event."</br>
+				<img border="0" alt="home" src="dolby.PNG" width=250 height=90/></br>
+				So do yourself a favour and bring your whole family down to Silverado Cinemas 
+				for the best family experience money can buy. Whatever you movie tastes, we will
+				have you covered!
+				</td>
+			</tr>
+
+		</table>
+		
+	</div>
+	</br>
+	<div id="box"
+	<div class="discount"> ON MONDAYS AND TUESDAYS (ALL DAY) </br> AND ON MONDAYS TO FRIDAYS (1PM SESSION ONLY) </br>GET DISCOUNTED TICKETS!</br>
+	</div>
+    </main>
+
+    <footer class="footer">
+      &copy; Lucas O'Hare s3545867
+      <script>
+        document.write(new Date().getFullYear());
+      </script>.
+    </footer>
+
+  </body>
+</html>
+
+
+
+
+
+
