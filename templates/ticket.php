d7666d1bd6879584cde37b56be3e57f3968c4589
Lucas002 <s3545867@student.rmit.edu.au>
2016-05-14 19:56:51 +1000

    Update ticket.php

--- templates/ticket.php
+++ templates/ticket.php
@@ -1,7 +1,38 @@
-<p>
-    Silverado<br />
-    <?= $args['day']?> <?= $args['time'] ?><br />
-    <?= $args['movie_title'] ?><br />
-    <?= $args['type']?> <br />
-</p>
+<style>
+#ticket{border: 3px solid black; 
+		border-collapse:collapse; 
+		width:400px; 
+		background-color: rgb(148,6,11);
+		color: white;
+		text-align:left;
+		padding:5px;
+		padding-left:15px;
+}
+.barcode{padding:30px;
+		 text-align:right;
+	
+}
+</style>
 
+
+<p>
+  <div id="ticket">
+	<table>
+	  <tr>
+	  
+		<th>
+		<div><img border="0" alt="home" src="static/img/logo.PNG" width=250 height=75/></div> <br />
+		<?= $args['movie_title'] ?><br />
+		<?= $args['day']?> <?= $args['time'] ?><br />
+		<?= $args['type']?> <br />
+		</th>
+		
+		<th class="barcode">
+		<img src="static/img/barcode.png" width= 50 height=125/>
+		</th>
+
+	  </tr>
+	</table>
+  </div>
+
+</p>
