be90dff62c60ba2cfe2af2b11b6886293aba5cdc
Lucas002 <s3545867@student.rmit.edu.au>
2016-05-14 19:57:16 +1000

    Update cart_item.php

--- templates/cart_item.php
+++ templates/cart_item.php
@@ -1,9 +1,12 @@
 <div>
+<fieldset clas="feildset">
     <p>
-	<?= $args['movie']->title ?>(<?= $args['movie']->rating ?>)<a href='cart.php?action=delete_item&i=<?= $args['item_number']?>'>Delete from cart</a>
+	<?= $args['movie']->title ?>(<?= $args['movie']->rating ?>)
 	
 	<br />
 	Showing at <?= $args['session']->day ?>, <?= $args['session']->time ?><br />
+	
+	
     </p>	
     <table>
 	<tr><th>Ticket Type</th><th>Cost</th><th>quantity</th><th>Subtotal</th></tr>
@@ -22,4 +25,6 @@
 	    <td>$<?= number_format($args['booking']->total,2) ?></td>
 	</tr>
     </table>
+	<a href='cart.php?action=delete_item&i=<?= $args['item_number']?>'>Delete from cart</a>
+	</fieldset>
 </div>
